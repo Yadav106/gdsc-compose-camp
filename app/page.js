f0eb4ff89c47b55e9369d9b035776d4ddf74d020
rizzabh <pandeyrishabh966@gmail.com>
2023-12-12 09:41:29 +0530

    Hero Section with Ui Elements

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@ import Hero from './components/Hero/Hero'
 import Contact from './components/Contact/Contact'
 import Faq from './components/Faq/Faq'
 import Footer from './components/Footer/Footer'
+import Camp from './components/Camp/Camp'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-2">
       <Header />
       <Hero />
+      <Camp/>
       <Contact />
       <Faq />
       <Footer />
