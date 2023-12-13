5994583a74d468841d94f938440932fe09dbb649
rizzabh <pandeyrishabh966@gmail.com>
2023-12-13 15:49:06 +0530

    Fixed meta data and dummy datas

--- app/components/Faq/Faq.jsx
+++ app/components/Faq/Faq.jsx
@@ -4,31 +4,45 @@ import Accordion from "./Accordion";
 const FAQ = () => {
   return (
     <div className="p-4 w-6/12 max-sm:w-9/12 m-4">
-      <h1 className="text-4xl font-bold text-center mt-4">FAQ's</h1>
+      <h1 className="text-4xl font-bold text-center mt-4" id="Faq">
+        FAQ's
+      </h1>
       <div className="text-gray-600 text-md text-center mt-2">
         Frequently asked questions about the Program
       </div>
       <Accordion
-        title="Do you prefer Android or iOS"
-        answer="I like to use iOS products"
+        title="What is jetpack compose ?"
+        answer="
+        Jetpack Compose is a modern, fully declarative UI toolkit for building native Android applications. It is a part of the Android Jetpack libraries and is designed to simplify and accelerate UI development on Android. Compose provides a concise and intuitive way to build user interfaces using Kotlin programming language."
       />
       <Accordion
-        title="Do you prefer writing CSS or Tailwind?"
-        answer="I like to use Tailwind"
+        title="What are the perks of this camp?"
+        answer="Learning Opportunities
+        Skill Development
+        Hands-On Experience
+        Networking
+        Community Engagement
+        Access to Experts
+        Resume Building
+        Project Showcase
+        Certificates and Recognition
+        Career Opportunities
+        Personal Growth
+        Fun and Engaging Environment"
       />
       <Accordion
-        title="Do you prefer writing CSS or Tailwind?"
-        answer="I like to use Tailwind"
+        title="Is this beginner Friendly?"
+        answer="Whether you’re a beginner or ready to master Jetpack Compose, learn about the latest development tools and APIs to build better Android apps.
+        "
       />
       <Accordion
-        title="Do you prefer writing CSS or Tailwind?"
-        answer="I like to use Tailwind"
+        title="Is this Camp Expensive?"
+        answer="No, its Absolutely Free!"
       />
       <Accordion
-        title="Do you prefer writing CSS or Tailwind?"
-        answer="I like to use Tailwind"
+        title="Is this Camp Online?"
+        answer="Yes, this camp is completely online"
       />
-      <Accordion title="Firebase or Supabase?" answer="I am using Supabase!" />
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import Contact from './components/Contact/Contact'
-import Faq from './components/Faq/Faq'
-import Footer from './components/Footer/Footer'
-import Camp from './components/Camp/Camp'
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Contact from "./components/Contact/Contact";
+import Faq from "./components/Faq/Faq";
+import Footer from "./components/Footer/Footer";
+import Camp from "./components/Camp/Camp";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between scroll-smooth">
       <Header />
       <Hero />
-      <Camp/>
+      <Camp />
       <Faq />
       <Contact />
       <Footer />
-      
     </main>
-  )
+  );
 }
