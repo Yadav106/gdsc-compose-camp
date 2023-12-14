9cab3e9160532216f91c8451b37465ded24f26d1
rizzabh <pandeyrishabh966@gmail.com>
2023-12-14 20:51:09 +0530

    Fixing minor issues

--- app/components/Camp/Camp.jsx
+++ app/components/Camp/Camp.jsx
@@ -5,12 +5,12 @@ const Camp = () => {
     <>
       <div className="w-full flex items-center justify-between bg-gradient-to-r from-gray-950 via-gray-500 to-gray-950 h-fit p-[1px] mt-20 lg:mt-48">
         <div className="h-fit w-full bg-gradient-to-b from-gray-950 to-[#0f011d] items-center justify-between px-2 py-6">
-          <div className="text-2xl mx-auto text-center font-semibold max-sm:text-lg">
+          <div className="text-2xl mx-auto text-center font-semibold max-sm:text-lg mb-2">
             Why did developers Participate in Compose Camp?
           </div>
           <div className="imgrid grid grid-cols-3 gap-1 px-24 m-4 max-[960px]:grid-cols-1 max-md:px-1 max-sm:m-1">
             <div className=" p-0">
-              <img src="/im2.png" alt="" className="mx-auto" />
+              <img src="/im2.png" alt="" className="mx-auto mb-2" />
               <div className="text-2xl font-semibold text-center m-1 max-sm:text-base ">
                 Learn The Latest Practices
               </div>
@@ -21,7 +21,7 @@ const Camp = () => {
               </div>
             </div>
             <div className=" p-0">
-              <img src="/im1.png" alt="" className="mx-auto" />
+              <img src="/im1.png" alt="" className="mx-auto mb-2" />
               <div className="text-2xl font-semibold text-center m-1 max-sm:text-base">
                 Apply your skills{" "}
               </div>
@@ -31,7 +31,7 @@ const Camp = () => {
               </div>
             </div>
             <div className=" p-0">
-              <img src="/im3.png" alt="" className="mx-auto" />
+              <img src="/im3.png" alt="" className="mx-auto mb-2" />
               <div className="text-2xl font-semibold text-center m-1 max-sm:text-base">
                 Connect with other developers{" "}
               </div>
